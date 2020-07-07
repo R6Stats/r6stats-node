c5792d43539a0c68be85f86192cfe9ac24f75f27
Jake Engel <hi@jengel.me>
2020-07-06 22:44:20 -0500

    chore: update dependencies and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint"],
   root: true,
   env: {
     node: true,
     es6: true
   },
   parserOptions: {
-    'ecmaVersion': 9,
-    parser: 'babel-eslint',
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  rules: {
+  rules:  {
+    'camelcase': 'off',
+    'quotes': [1, 'single'],
     'semi': ['error', 'never'],
-    'no-mixed-spaces-and-tabs': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
-    'rest-spread-spacing': ['error', 'never'],
-    'arrow-spacing': ['error', { 'before': true, 'after': true }],
-    'func-call-spacing': ['error', 'never'],
-    'no-multi-spaces': 'error',
-    'no-unused-vars': ['error', { 'vars': 'all', 'args': 'none', 'ignoreRestSiblings': true }],
-  }
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-unused-vars': ['error', { 'ignoreRestSiblings': true }],
+    '@typescript-eslint/camelcase': ['off'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off'
+  },
 }
